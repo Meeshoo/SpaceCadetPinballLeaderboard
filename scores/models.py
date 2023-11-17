0d820ab09af6bf8d764a70852dffb3f6dd698584
unknown <mitch1080@hotmail.co.uk>
2023-11-17 22:37:24 +0000

    Tidy up

--- scores/models.py
+++ scores/models.py
@@ -2,50 +2,9 @@ from django.db import models
 import json
 
 class scores(models.Model):
-    playerid = models.IntegerField(default=None, blank=True, null=True)
     name = models.TextField(max_length=30)
     score = models.IntegerField()
-	
-
-    # def getPlayerID(nameToGet):
-        
-    #     # Get objects with name and put into list
-    #     scoresForPlayer = list(scores.objects.filter(name = nameToGet))
-
-    #     # take one (first I guess)
-    #     item = scoresForPlayer[0]
-
-    #     # Get ID associated with name
-    #     playerID = item.playerid
-
-    #     #return ID
-	
-    # NOT FINISHED YET
-    def setPlayerID(playerName):
-
-        if playerName == "Mitch":
-    
-            scoresForPlayer = scores.objects.filter(name = "Mitch")
-
-            for s in scoresForPlayer:
-                s.playerid = 1
-                s.save()
-
-        if playerName == "Reggo":
-    
-            scoresForPlayer = scores.objects.filter(name = "Reggo")
-
-            for s in scoresForPlayer:
-                s.playerid = 2
-                s.save()
-        
-        if playerName == "Defaultsound":
-        
-            scoresForPlayer = scores.objects.filter(name = "Defaultsound")
-
-            for s in scoresForPlayer:
-                s.playerid = 3
-                s.save()
+    date = models.TextField(max_length=50)
 
     def submitData(postData):
 
@@ -105,39 +64,14 @@ class scores(models.Model):
         objectList = list(retreivedObjects)
         objectList.sort(reverse = True, key = scoreSort)
 
-        # Put scores into list
-        for o in objectList:
-            n = o.name
-            s = f'{o.score:,}'
-            retrievedNames.append(n)
-            retrievedScores.append(s)
-            
-        # Return top 10
-        if len(retrievedNames) > 1:
-            name1 = retrievedNames[0]
-            score1 = retrievedScores[0]
-            name2 = retrievedNames[1]
-            score2 = retrievedScores[1]
-            name3 = retrievedNames[2]
-            score3 = retrievedScores[2]
-            name4 = retrievedNames[3]
-            score4 =retrievedScores[3]
-            name5 = retrievedNames[4]
-            score5 = retrievedScores[4]
+        objectList = objectList[:10]
 
-            name6 = retrievedNames[5]
-            score6 = retrievedScores[5]
-            name7 = retrievedNames[6]
-            score7 = retrievedScores[6]
-            name8 = retrievedNames[7]
-            score8 = retrievedScores[7]
-            name9 = retrievedNames[8]
-            score9 =retrievedScores[8]
-            name10 = retrievedNames[9]
-            score10 = retrievedScores[9]
-			
+        count = 1
 
-            return name1,score1,name2,score2,name3,score3,name4,score4,name5,score5,name6,score6,name7,score7,name8,score8,name9,score9,name10,score10
+        for entry in objectList:
+            entry.score = "{:,}".format(entry.score)
+            entry.rank = count
+            count += 1
 
-        else:
-            return "Not enough scores in DB"
+        return objectList
+        
--- scores/views.py
+++ scores/views.py
@@ -6,44 +6,18 @@ from .models import scores
 from django.views.decorators.csrf import csrf_exempt
 
 dataSent = False
-name1 = "Not"
-score1 = 0
-name2 = "enough"
-score2 = 0
-name3 = "scores"
-score3 = 0
-name4 = "in"
-score4 = 0
-name5 = "database"
-score5 = 0
-name6 = "please"
-score6 = 0
-name7 = "submit"
-score7 = 0
-name8 = "10"
-score8 = 0
-name9 = "or more"
-score9 = 0
-name10 = "scores"
-score10 = 0
-
 
 @csrf_exempt
 def index(request):
 
-    global dataSent
-    global name1,score1,name2,score2,name3,score3,name4,score4,name5,score5,name6,score6,name7,score7,name8,score8,name9,score9,name10,score10
-
     if request.method == 'GET':
 
-        if scores.retrieveData() != "Not enough scores in DB":
-            name1,score1,name2,score2,name3,score3,name4,score4,name5,score5,name6,score6,name7,score7,name8,score8,name9,score9,name10,score10 = scores.retrieveData()
+        data = scores.retrieveData()
 
-        return TemplateResponse(request, 'index.html', {"n1":name1, "s1":score1, "n2":name2, "s2":score2, "n3":name3, "s3":score3, "n4":name4, "s4":score4, 
-        "n5":name5, "s5":score5, "n6":name6, "s6":score6, "n7":name7, "s7":score7, "n8":name8, "s8":score8, "n9":name9, "s9":score9, 
-        "n10":name10, "s10":score10})
+        return TemplateResponse(request, 'index.html', {"data":data})
 
     elif request.method == 'POST':
+
         data = request.body
 
         scores.submitData(data)
@@ -52,12 +26,6 @@ def index(request):
         dataSent = True
 
         return HttpResponse("Post")
-
-
-
-
-
-
 
         # WORKING POST
         #score.scores = request.body
